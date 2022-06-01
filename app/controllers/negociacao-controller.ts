75671e98c333b67775eb34c40ac92ed881d2d916
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2022-06-01 19:01:26 -0300

    Aula 03 - Atividade 02: O controller de negociação

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,15 @@
+export class NegociacaoController {
+  private _inputData;
+  private _inputQuantidade;
+  private _inputValor;
+
+  constructor() {
+    this._inputData = document.getElementById('data');
+    this._inputQuantidade = document.getElementById('quantidade');
+    this._inputValor = document.getElementById('valor');
+  }
+
+  adiciona() {
+    
+  }
+}
