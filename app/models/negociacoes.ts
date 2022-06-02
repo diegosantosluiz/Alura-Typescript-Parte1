3a6b4a72059d502b60df8610ab10008e5ce374cb
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2022-06-02 17:11:50 -0300

    Aula 04 - Atividade 05: Avançando com nosso modelo

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,5 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = [];
+  private _negociacoes: Array<Negociacao> = [];
+
+  adicionar(negociacao: Negociacao): void {
+    this._negociacoes.push(negociacao);
+  }
+
+  lista(): Array<Negociacao> {
+    return this._negociacoes;
+  }
 }
